7e79acc1c15f3d5945adfb8253bcca9669129969
KeisukeKurosaki <Cglanville4645@conestogac.on.ca>
2023-12-08 13:13:21 -0500

    Added comments to all pages. Still need additional comments on the css styling web page and master page.

--- FinalTest/Finished.aspx
+++ FinalTest/Finished.aspx
@@ -1,4 +1,15 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Finished.aspx.cs" Inherits="FinalTest.Page4" %>
+﻿<%-- 
+FILE		    : Finished.aspx
+PROJECT			: PROG 2001 - Final Exam Practical Component
+PROGRAMMERS		: Joey Kingston, Cody Glanville
+FIRST VERSION   : December 03, 2023
+DESCRIPTION		:
+	This file is used in order to display the status of a user's order for the SET Pizza Shop. Depending on the user's choice on the previous
+    page of either confirming or cancelling their order, they will be prompted in this page with a thankyou using their full name, and also the 
+    status of their order (confirmed or cancelled).
+--%>
+
+<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Finished.aspx.cs" Inherits="FinalTest.Finished" %>
 
 
 
--- FinalTest/Finished.aspx.cs
+++ FinalTest/Finished.aspx.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+* FILE				: Finished.aspx.cs
+* PROJECT			: PROG 2001 - Final Exam Practical Component
+* PROGRAMMERS		: Joey Kingston, Cody Glanville
+* FIRST VERSION		: December 03, 2023
+* DESCRIPTION		:
+*	This file is used for the server-sided logic for the final web page in the SET Pizza Shop application. At the moment, their is currently
+*	not code-behind that was created to be added to this file outside of the automatically generated method of Page_Load().
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,8 +17,27 @@ using System.Web.UI.WebControls;
 
 namespace FinalTest
 {
-    public partial class Page4 : System.Web.UI.Page
+    /*
+    * PUBLIC PARTIAL CLASS         : Finished
+    * DESCRIPTION	               :
+    *	This class acts to hold the server-sided methods for our final web page used for the SET Pizza Shop. As of right now, there are no
+    *	additional methods or logic that has been added or needs to be added to this final page outside of the automatically created Page_Load() 
+    *	method. 
+    */
+    public partial class Finished : System.Web.UI.Page
     {
+        /*
+        *	METHOD          : Page_Load(object sender, EventArgs e)
+        *	DESCRIPTION		:
+        *		This method is automatically called upon and acts as an event handler in the page lifecycle. This method ultimately acts to
+        *		perform tasks when a webpage isloaded. In this particular case, no additional logic was needed in this method relating to 
+        *		any type of validation, etc.
+        *	PARAMETERS      :
+        *		object sender            :   This is the object that triggered the event for the page to load
+        *		EventArgs e              :   This is an object that contains event-specific data
+        *	RETURNS			:
+        *		void	                 :	 This method does not have any return value
+        */
         protected void Page_Load(object sender, EventArgs e)
         {
 
