e88a4d0c0352f23fd503c45589e3ce66b25353f3
KeisukeKurosaki <Cglanville4645@conestogac.on.ca>
2023-12-09 14:20:31 -0500

    Final headers added to the master page.

--- FinalTest/Site.Master.cs
+++ FinalTest/Site.Master.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+* FILE				: Site.Master.cs
+* PROJECT			: PROG 2001 - Final Exam Practical
+* PROGRAMMERS		: Joey Carson (ID: 8213035, Lab Section: 6), Cody Glanville (ID: 8864645, Lab Section: 3)
+* FIRST VERSION		: December 3, 2023
+* DESCRIPTION		:
+*	This file contains the server-side logic for the Site.Master.cs page. However, this file is not currently used
+*	or has been modified outside of its initial creation at the start of our assignment. 
+*/
+
+using System;
 
 namespace Final
 {
